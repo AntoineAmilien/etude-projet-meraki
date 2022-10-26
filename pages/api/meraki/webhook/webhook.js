befdb9f0ed227936395b74774b49f1768e498958
Antoine AMILIEN <antoine.amilien@nxo.eu>
2022-10-26 16:56:00 +0200

    [RC] api meraki webhook

--- pages/api/meraki/webhook/webhook.js
+++ pages/api/meraki/webhook/webhook.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
         case 'GET':
             try {
                 console.log("API WEBHOOK MERAKI call : " + req)
-                res.status(201).json({ success: true, message: "La configuration a bien été modifiée." })
+                res.status(201).json({ success: true, message: "Retour de l'api meraki" })
             } catch (error) {
                 console.error(error)
                 res.status(500).json({ success: false, message: "Une erreur c'est produite pendant le traitement du signal webhook meraki" })
