532481567a109562435734298229d61f54deeba2
Antoine AMILIEN <antoine.amilien@noc.local>
2023-01-11 16:58:59 +0100

    [FEAT] push d'un mail avec webhook alerte

--- pages/api/meraki/webhook/webhook.js
+++ pages/api/meraki/webhook/webhook.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 export default async function handler(req, res) {
     const { method, body } = req;
@@ -5,14 +6,48 @@ export default async function handler(req, res) {
     switch (method) {
         case 'POST':
             try {
-
                 console.log("API WEBHOOK MERAKI call body: " + JSON.stringify(body))
+
+                const objetMail = {
+                    "auth_key": "MsFzurfZmN", //token de dev
+                    "to": "antoine.amilien@nxo.eu",
+                    "subject": `[MERAKI ALERTE] ${body.organizationName} sur le site : ${body.networkName} `,
+                    "content": [{
+                        "type": "HEAD",
+                        "title": `[MERAKI ALERTE] ${body.organizationName} sur le site : ${body.networkName} `,
+                        "message": ""
+                    },
+                    {
+                        "type": "BODY",
+                        "title": "Contenu de l'alerte : ",
+                        "message": `${JSON.stringify(body)}`
+                    },
+                    {
+                        "type": "BODY",
+                        "title": "le titre du body 2",
+                        "message": "le message du body 2",
+                        "buttonMessage": "button body 2",
+                        "buttonLink": "https://google.com"
+                    },
+                    {
+                        "type": "CONTACT",
+                        "telephone": "0612121212",
+                        "mail": "foo@bar.qux"
+                    }
+                    ],
+                    "template": "BASE"
+                };
+
+                await axios.post('https://service-nxo-smtp.digitalview-dev.nxo.eu/api/mail', objetMail)
+                    .then(function (response) {
+                        console.log("reponse deu axios qui push le mail : " + response);
+                    })
+
                 res.status(201).json({ success: true, message: "Retour de l'api meraki" })
             } catch (error) {
                 console.error(error)
                 res.status(500).json({ success: false, message: "Une erreur c'est produite pendant le traitement du signal webhook meraki" })
             }
-
             break;
     }
 
