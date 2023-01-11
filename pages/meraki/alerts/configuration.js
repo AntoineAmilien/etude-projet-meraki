21606b8545c14828d4471832f4b3cda30f9c84ef
AntoineAmilien <47785228+AntoineAmilien@users.noreply.github.com>
2023-01-11 16:35:13 +0100

    Update configuration.js

--- pages/meraki/alerts/configuration.js
+++ pages/meraki/alerts/configuration.js
@@ -3,18 +3,21 @@ import { axios_instance_apiMeraki } from "../../../utils/meraki/axios"
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import axios from 'axios';
+import Spinner from "react-spinners/PacmanLoader";
 
 const Configuration = (props) => {
     const { organizationId, organizationName, alertsConfiguration } = props;
     const router = useRouter()
     const [message, setMessage] = useState(null)
+    const [displaySpinner, setDisplaySpinner] = useState(false)
 
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmitConfiguration = async data => {
         try {
+            setDisplaySpinner(true)
             const baseObject = {
-                type: "portDown",
-                enabled: data.portDown,
+                type: "applianceDown",
+                enabled: data.applianceDown,
                 alertDestinations: {
                     emails: [],
                     snmp: false,
@@ -22,17 +25,19 @@ const Configuration = (props) => {
                     httpServerIds: []
                 },
                 filters: {
-                    timeout: 60,
-                    selector: "any port"
+                    timeout: 5,
                 }
             }
 
-            axios.post("/api/meraki/alerts/configuration", { "organizationId": organizationId, "putObjects": [baseObject] }).catch(_error => setMessage({ type: "error", message: "Une erreur c'est produite." }))
-
+            await axios.post("/api/meraki/alerts/configuration", { "organizationId": organizationId, "putObjects": [baseObject] })
+                .catch(_error => setMessage({ type: "error", message: "Une erreur c'est produite." }))
+            setDisplaySpinner(false)
+            setMessage({ type: "success", message: "La nouvelle configuration a bien été prise en compte." })
         } catch (_error) {
             setMessage({ type: "error", message: "Une erreur c'est produite." })
+            setDisplaySpinner(false)
         }
-        setMessage({ type: "success", message: "La nouvelle configuration a bien été prise en compte." })
+
     };
 
     return (
@@ -63,29 +68,35 @@ const Configuration = (props) => {
                                     <form onSubmit={handleSubmit(onSubmitConfiguration)}>
                                         <div className="flex flex-col w-full border-opacity-50">
                                             <div className="divider">Network-wide</div>
-                                            <div className="grid h-20 card bg-base-300 rounded-box place-items-center">...</div>
-                                            <div className="divider">Wireless</div>
-                                            <div className="grid h-20 card bg-base-300 rounded-box place-items-center">...</div>
-                                            <div className="divider">Security appliance</div>
-                                            <div className="grid h-20 card bg-base-300 rounded-box place-items-center">...</div>
-                                            <div className="divider">Switch</div>
                                             <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
                                                 <ul>
                                                     <li>
                                                         <div className="form-control">
                                                             <label className="label cursor-pointer">
-                                                                <input type="checkbox" className="toggle toggle-primary" {...register("portDown", { value: alertsConfiguration.alerts.filter(el => { return el.type === "portDown" })[0].enabled })} />
-                                                                <span className="ml-4 label-text">Any port goes down for more than 60 minutes</span>
+                                                                <input type="checkbox" className="toggle toggle-primary" {...register("applianceDown", { value: alertsConfiguration.alerts.filter(el => { return el.type === "applianceDown" })[0].enabled })} />
+                                                                <span className="ml-4 label-text">A security appliance goes offline for 5 minutes</span>
                                                             </label>
                                                         </div>
                                                     </li>
                                                 </ul>
                                             </div>
+                                            <div className="divider">Wireless</div>
+                                            <div className="grid h-20 card bg-base-300 rounded-box place-items-center">...</div>
+                                            <div className="divider">Security appliance</div>
+                                            <div className="grid h-20 card bg-base-300 rounded-box place-items-center">...</div>
+                                            <div className="divider">Switch</div>
+                                            <div className="grid h-20 card bg-base-300 rounded-box place-items-center">...</div>
                                         </div>
                                         <div className="flex mt-4 justify-end">
                                             <div>
-                                                <input type="submit" className="btn btn-primary" value="Sauvegarder cette configuration" />
-                                                {message && <p className={message.type == "success" ? "text-green-500" : "text-red-500"}>{message.message}</p>}
+                                                {displaySpinner === false ? <>
+                                                    <input type="submit" className="btn btn-primary" value="Sauvegarder cette configuration" />
+                                                    {message && <p className={message.type == "success" ? "text-green-500" : "text-red-500"}>{message.message}</p>}
+                                                </> : <div className='flex mr-10'>
+                                                    <p className='text-neutral mr-5'>Merci de patienter quelques instants : </p>
+                                                    <Spinner color="#570DF8" size={10} />
+                                                </div>}
+
                                             </div>
                                         </div>
                                     </form>
